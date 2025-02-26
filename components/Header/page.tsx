89c9f3854e53550161bebc6ed7cda9a3ee344502
selman13 <selmaneliyev99@gmal.com>
2025-02-26 13:37:29 +0400

    Header cmponents edited

--- components/Header/page.tsx
+++ components/Header/page.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Loqo from '../common/Loqo'
+import React from "react";
+import Loqo from "../common/Loqo";
+import Slogan from "../common/Slogan";
+import Link from "next/link";
+import MobileNav from "../common/MobileNav";
 
 const Header = () => {
   return (
-    <div className='bg-red-800 p-2 flex gap-10'>
-        <Loqo/>
-    </div>
-  )
-}
+    <div className="bg-red-800 p-2 flex gap-5 justify-between items-center text-white">
+      <Loqo />
+      <div className="hidden mx-5 gap-10 md:flex">
+        <Link href="/books">Kitablar</Link>
+        <Link href="/basket">Səbət</Link>
+        <Link href="/login">Giriş</Link>
+      </div>
 
-export default Header
+      <div className="flex md:hidden">
+        <MobileNav />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
