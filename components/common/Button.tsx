1841ec4ea6366ed102e6bb9463cc4b6d6d169412
selman13 <selmaneliyev99@gmal.com>
2025-02-26 13:36:38 +0400

    common files created and edited

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -4,7 +4,7 @@ import {IButtonTypes} from '../../Types/TButtonTypes';
 const Button: React.FC<IButtonTypes> = ({ text }) => {
   return (
     <div>
-      <button className="bg-red-800 text-white rounded w-[300px] p-2 mx-3">
+      <button className="bg-red-800 text-white rounded w-[300px] p-2 mx-3 border hover:bg-white hover:text-red-800 hover:border-4">
         {text}
       </button>
     </div>
--- components/common/Loqo.tsx
+++ components/common/Loqo.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Loqo = () => {
   return (
-    <div className='font-bold text-2xl text-white p-2'>Loqo</div>
+    <div className='font-bold text-2xl text-white p-2'>
+      <Link href="/">LOQO</Link>
+    </div>
   )
 }
 
