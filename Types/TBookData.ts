8aabc03bd8b9b6827a678f4076093f21852f9df5
selman13 <selmaneliyev99@gmal.com>
2025-02-28 23:03:02 +0400

    Books datas created

---
+++ Types/TBookData.ts
@@ -0,0 +1,13 @@
+export interface IBookData {
+  bookID: number;
+  bookName: string;
+  bookAuthor: string;
+  bookDescription: string;
+  bookPhoto: string;
+  bookGenre: string | string[];
+  bookPrice: number;
+  bookYear: number;
+  bookPage: number;
+  bookCount: number;
+  id: string;
+}
