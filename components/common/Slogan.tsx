c41d18133701c6a476a8f508df35b7d59ba2a9dc
selman13 <selmaneliyev99@gmal.com>
2025-02-28 23:03:28 +0400

    common components edited

--- components/common/Slogan.tsx
+++ components/common/Slogan.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Slogan = () => {
   return (
     <div>
-        <p className='text-white w-full'>AZƏRBAYCANIN KİTAB SFERASINA XOŞ GƏLMİSİNİZ</p>
+        <p className='text-white w-full tracking-normal md:tracking-[10px] '>AZƏRBAYCANIN KİTAB SFERASINA XOŞ GƏLMİSİNİZ</p>
     </div>
   )
 }
