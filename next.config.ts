902d94f53595cd37864b4e223d956ed9393e6f63
selman13 <selmaneliyev99@gmal.com>
2025-02-28 23:02:40 +0400

    books path added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "static.insales-cdn.com",
+      "kitabal.az",
+      "www.qanun.az",
+      "cdn.dsmcdn.com"
+    ],
+  },
 };
 
 export default nextConfig;
