653afab78626fbbc691b80a400a217035388e4f6
selman13 <selmaneliyev99@gmal.com>
2025-03-01 21:35:50 +0400

    common file edited

--- components/common/Search.tsx
+++ components/common/Search.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { ISearch } from "@/Types/TSearch";
 
-const Search = () => {
+const Search = ({searchText, setSearchText}: ISearch) => {
   return (
-    <div className='flex flex-1'>
-        <input type="search" placeholder='Axtar' className='flex-1 border-1 p-2 rounded-md outline-black text-black'/>
+    <div className="flex flex-1">
+      <input
+        type="search"
+        placeholder="Axtar"
+        className="flex-1 border-1 p-2 my-2 mx-2 bg-red-900 rounded-md outline-red-900 text-white"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
