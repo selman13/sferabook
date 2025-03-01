20e83f655a7fc806dc2ea13a37bce35faaf1993c
selman13 <selmaneliyev99@gmal.com>
2025-03-01 21:36:08 +0400

    Header component edited

--- components/Header/page.tsx
+++ components/Header/page.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import Loqo from "../common/Loqo";
 import Link from "next/link";
 import MobileNav from "../common/MobileNav";
-import Search from "../common/Search";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
   return (
     <div className="bg-red-800 p-2 flex gap-5 justify-between items-center text-white">
       <Loqo />
-      <Search/>
+
       <div className="hidden mx-5 gap-10 md:flex">
         <Link href="/books">Kitablar</Link>
         <Link href="/basket">Səbət</Link>
